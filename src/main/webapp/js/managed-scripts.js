bc2735bcb066673425a73a7646a07f6b3a0f5d67
imod <domi@fortysix.ch>
2012-10-11 19:13:42 +0200

    fix issue if jenkins is started with a different root context

--- src/main/webapp/js/managed-scripts.js
+++ src/main/webapp/js/managed-scripts.js
@@ -1,5 +1,5 @@
 
-function ms_initDetailLink(referenceTag){
+function ms_initDetailLink(rootURL, referenceTag){
    var selId = referenceTag.value;
    var all = new Array();
    all = document.getElementsByName('buildStepId');
@@ -7,7 +7,7 @@ function ms_initDetailLink(referenceTag){
 	   if(referenceTag == all.item(i)){
 		   var detailsLinkTag = document.getElementsByName('showDetailLink').item(i);
 		   if(selId.length != 0){
-			   detailsLinkTag .href="/configfiles/show?id=".concat(selId);
+			   detailsLinkTag .href=rootURL+"/configfiles/show?id=".concat(selId);
 			   detailsLinkTag .style.display = 'block';
 			}else{
 			   detailsLinkTag .style.display = 'none';
